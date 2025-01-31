953c6af02b138d6d2d9eded76b30fe6ff0da3579
João Correia <joaocorreiaprof@gmail.com>
2025-01-31 13:47:50 +0000

    Added features to also check if user found the other characters

--- server/controllers/waldoController.js
+++ server/controllers/waldoController.js
@@ -1,4 +1,3 @@
-//waldo area
 let waldoArea = {
   xMin: 41.0,
   xMax: 44.0,
@@ -6,26 +5,62 @@ let waldoArea = {
   yMax: 22.0,
 };
 
+let dogArea = {
+  xMin: 62.0,
+  xMax: 66.0,
+  yMin: 70.0,
+  yMax: 75,
+};
+
+let evilArea = {
+  xMin: 62.0,
+  xMax: 66.0,
+  yMin: 81.0,
+  yMax: 93.0,
+};
+
 module.exports = {
   validateSelection: async (req, res) => {
     const { x, y, character } = req.body;
 
-    //First, checks if user selected different than waldo
-    if (character !== "waldo") {
-      return res.json({ message: "❌ Wrong choice! Try again.", found: false });
+    if (character === "waldo") {
+      const isInsideX = x >= waldoArea.xMin && x <= waldoArea.xMax;
+      const isInsideY = y >= waldoArea.yMin && y <= waldoArea.yMax;
+
+      if (isInsideX && isInsideY) {
+        return res.json({ message: "🎉 You found Waldo!", found: true });
+      } else {
+        return res.json({
+          message: "❌ Not quite! Try again.",
+          found: false,
+        });
+      }
     }
+    if (character === "dog") {
+      const isInsideX = x >= dogArea.xMin && x <= dogArea.xMax;
+      const isInsideY = y >= dogArea.yMin && y <= dogArea.yMax;
 
-    //Checks if user clicked inside waldo
-    const isInsideX = x >= waldoArea.xMin && x <= waldoArea.xMax;
-    const isInsideY = y >= waldoArea.yMin && y <= waldoArea.yMax;
+      if (isInsideX && isInsideY) {
+        return res.json({ message: "🎉 You found Dog!", found: true });
+      } else {
+        return res.json({
+          message: "❌ Not quite! Try again.",
+          found: false,
+        });
+      }
+    }
+    if (character === "evil") {
+      const isInsideX = x >= evilArea.xMin && x <= evilArea.xMax;
+      const isInsideY = y >= evilArea.yMin && y <= evilArea.yMax;
 
-    if (isInsideX && isInsideY) {
-      return res.json({ message: "🎉 You found Waldo!", found: true });
-    } else {
-      return res.json({
-        message: "❌ Not quite! Try again.",
-        found: false,
-      });
+      if (isInsideX && isInsideY) {
+        return res.json({ message: "🎉 You found Evil Man!", found: true });
+      } else {
+        return res.json({
+          message: "❌ Not quite! Try again.",
+          found: false,
+        });
+      }
     }
   },
 };
