97667b7258fe29bc28e3093f0eb133b1cb46de2f
João Correia <joaocorreiaprof@gmail.com>
2025-02-03 14:37:55 +0000

    Update packages for deploy

--- server/index.js
+++ server/index.js
@@ -1,6 +1,4 @@
-require("dotenv").config();
 const express = require("express");
-const cors = require("cors");
 
 const app = express();
 const PORT = process.env.PORT || 5000;
@@ -9,8 +7,6 @@ const PORT = process.env.PORT || 5000;
 const waldoRoutes = require("./routes/waldoRoutes");
 const marioRoutes = require("./routes/marioRoutes");
 const robotRoutes = require("./routes/robotRoutes");
-
-app.use(cors());
 
 //Middleware
 app.use(express.json());
@@ -24,6 +20,16 @@ app.get("/", (req, res) => {
 app.use("/api/waldo", waldoRoutes);
 app.use("/api/mario", marioRoutes);
 app.use("/api/robot", robotRoutes);
+
+// Serve static files from the client/dist directory
+const clientBuildPath = path.join(__dirname, "../../client/dist"); // Adjust path for deployment
+app.use(express.static(clientBuildPath));
+
+// Handle all other routes with the frontend's index.html
+app.get("*", (req, res) => {
+  res.sendFile(path.join(clientBuildPath, "index.html"));
+  console.log("Static files served from:", clientBuildPath);
+});
 
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
