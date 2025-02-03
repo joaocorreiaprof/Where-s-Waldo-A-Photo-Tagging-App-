b1ccd0e41991fc9ac6127956cff27cfd7d236482
João Correia <joaocorreiaprof@gmail.com>
2025-02-03 16:05:41 +0000

    deployment

--- server/index.js
+++ server/index.js
@@ -14,7 +14,6 @@ app.use(cors());
 app.use(express.json());
 
 app.use(express.static(path.join(__dirname, "../client/dist")));
-
 app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "../client/dist/index.html"));
 });
